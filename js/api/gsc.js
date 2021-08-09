c0f318944566266ea3c35be5d6bc6d86921d9d8f
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-08-09 14:15:21 +0530

    Entire code.

---
+++ js/api/gsc.js
@@ -0,0 +1,18 @@
+async function gscApi(link, api) {
+	const parameters = { key: api.key }, url = setUpQuery(link, api.url, parameters), result = {};
+
+	const response = await axios.post(url, {
+		"url": link,
+		"requestScreenshot": false
+	});
+
+	const json = response.data;
+	result['Status'] = json['mobileFriendliness'];
+	result['Issues'] = [];
+
+	if (json.mobileFriendlyIssues) {
+		result['Issues'] = json['mobileFriendlyIssues'];
+	}
+
+	return {...result};
+};
